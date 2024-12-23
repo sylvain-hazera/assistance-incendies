1e2ed29f8a3910aa21f9d097b73b20447008cb28
sylvain-hazera <72251961+sylvain-hazera@users.noreply.github.com>
2024-12-23 14:31:13 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,6 +1,6 @@
 <?php
    try{
-      $pdo=new PDO("mysql:host=db5014786741.hosting-data.io;dbname=dbs12286113","dbu1192794","!Denkor1504@-D@nsl3srid3@uX");
+      $pdo=new PDO("mysql:host=;dbname=","","");
    }
    catch(PDOException $e){
       echo $e->getMessage();
